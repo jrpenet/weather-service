8dc23f67910f0ebca0507ee495d54c72339927d1
jrpenet <gilnyor@gmail.com>
2020-08-30 21:59:20 -0300

    concat the air humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,8 @@ const forecast = (latitude, longitude, callback) => {
         }else if(body.error){
             callback('Unable to find location. Try another search', undefined)
         }else{
-            callback(undefined, body.current.weather_descriptions[0] + `. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out.`)
+            console.log(body.current)
+            callback(undefined, body.current.weather_descriptions[0] + `. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out. The air humidity is ${body.current.humidity}%`)
         }
     })
 }
